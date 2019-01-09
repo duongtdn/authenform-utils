8a60bae9003974aa8ce11381b62bf996beda2748
duongtdn <duongtdn@gmail.com>
2019-01-09 07:25:20 +0700

    add post function

--- xhttp.js
+++ xhttp.js
@@ -9,6 +9,16 @@ module.exports = {
       done(request.status, request.responseText)
     })  
     request.send();
+  },
+
+  post(url, data, done) {
+    const request = new XMLHttpRequest()
+    request.open('POST', url, true)
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.addEventListener('load', () => {
+      done(request.status, request.responseText)
+    })  
+    request.send(JSON.stringify(data));
   }
 
 }
