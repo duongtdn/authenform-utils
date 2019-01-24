2f43742103889047bf1cbca6bbea4d4d8ba4a3ed
duongtdn <duongtdn@gmail.com>
2019-01-24 23:15:28 +0700

    put allow options authen

--- src/xhttp.js
+++ src/xhttp.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const { isFunction, isObject } = require('./typecheck')
+const storage = require('./localstorage')
+
 module.exports = {
 
   get(url, done) {
@@ -21,10 +24,16 @@ module.exports = {
     request.send(JSON.stringify(data));
   },
 
-  put(url, data, done) {
+  put(url, data, options, done) {
+    if (isFunction(options)) {
+      done = options
+    }
     const request = new XMLHttpRequest()
     request.open('PUT', url, true)
     request.setRequestHeader('Content-Type', 'application/json');
+    if (isObject(options) && options.authen && storage.get('token')) {
+      request.setRequestHeader('Authorization', `Bearer ${storage.get('token')}`);
+    }
     request.addEventListener('load', () => {
       done(request.status, request.responseText)
     })  
