e2961492b82ee4fc28bd5345e0608500f9062781
duongtdn <duongtdn@gmail.com>
2019-01-24 23:15:44 +0700

    export new functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict"
 
-const formCheck = require('./src/form-check')
+const formCheck = require('./src/formcheck')
 const xhttp = require('./src/xhttp')
+const typecheck = require('./src/typecheck')
+const storage = require('./src/localstorage')
 
-module.exports = { ...formCheck, xhttp }
+module.exports = { ...formCheck, xhttp, storage, ...typecheck }
